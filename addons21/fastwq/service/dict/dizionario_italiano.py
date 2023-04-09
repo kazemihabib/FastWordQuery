25b9d3cb2ecc6e50d077eb04e2378a3cc759445e
Habib Kazemi <kazemihabib1996@gmail.com>
2023-04-09 23:27:13 +0330

    Add table and css to dizionario_italiano

--- addons21/fastwq/service/dict/dizionario_italiano.py
+++ addons21/fastwq/service/dict/dizionario_italiano.py
@@ -11,6 +11,7 @@ from aqt.sound import av_player
 
 dizionario_italiano_base_url = u'https://www.dizionario-italiano.it/dizionario-italiano.php?parola={}100'
 
+
 @register([u'dizionario_italiano', u'Dizionario Italiano'])
 class dizionarioItaliano(WebService):
 
@@ -30,6 +31,7 @@ class dizionarioItaliano(WebService):
                       'pronuncia': self.__get_pronuncia(soup),
                       'grammatica': self.__get_grammatica(soup),
                       'soundlink': self.__get_sound_link(soup),
+                      'table': self.__get_table(soup),
                       'url': self._get_url()}
         except Exception as e:
             print('exception is ', e)
@@ -39,6 +41,7 @@ class dizionarioItaliano(WebService):
                       'pronuncia': '',
                       'grammatica': '',
                       'soundlink': '',
+                      'table': '',
                       'url': ''}
         return self.cache_this(result)
 
@@ -47,7 +50,7 @@ class dizionarioItaliano(WebService):
         seg = self._get_field('pronuncia')
         print(seg)
         return seg
-    
+
     def __fld_mp3(self):
         audio_url = self._get_field('soundlink')
         if audio_url:
@@ -67,14 +70,18 @@ class dizionarioItaliano(WebService):
             return '\n'.join(self._get_field('definizione'))
         except Exception():
             return ""
-    
+
     @export('Sillabe')
     def fld_sillabe(self):
         return str(self._get_field('sillabe'))
 
-    @export('Grammatica') 
+    @export('Grammatica')
     def fld_grammatica(self):
         return ' '.join(self._get_field('grammatica'))
+
+    @export('table')
+    def fld_table(self):
+        return self._get_field('table')
 
     def __get_lemma(self, soup):
         lemma = soup.find('span', class_='lemma')
@@ -110,7 +117,6 @@ class dizionarioItaliano(WebService):
         gram = soup.find_all('span', class_="grammatica")
         return [x.text for x in gram]
 
-
     def __generate_current_date(self):
         today = datetime.now()
         date = str(today.year) + str(today.month) + str(today.day)
@@ -120,9 +126,14 @@ class dizionarioItaliano(WebService):
 
     def __get_sound_link(self, soup):
         mp3_path = soup.find('span', class_="psPlay")['data-audio'].strip()
-        sound_url = "https://www.dizionario-italiano.it/{0}&sc=1&d={1}".format(mp3_path, self.__generate_current_date())
+        sound_url = "https://www.dizionario-italiano.it/{0}&sc=1&d={1}".format(
+            mp3_path, self.__generate_current_date())
         print('data_audio', sound_url)
         return sound_url
+
+    def __get_table(self, soup):
+        table = soup.find('div', {'class': ['section', 'group', 'sec-top']})
+        return self._css(str(table))
 
     def __get_locuzioni(self, soup):
         bad_loc = soup.find_all('span', class_='cit_ita_1')
@@ -153,3 +164,137 @@ class dizionarioItaliano(WebService):
         if len(defs) == 0:
             raise exceptions.WordNotFoundError()
         return defs
+
+    # @with_styles(need_wrap_css=True, cssfile='_dizionario_italiano.css') //adding cssfile didn't work so I added the css for tables manually
+    def _css(self, val):
+        return ''' <style> 
+/* Responsive tables */
+.section {              /*  SECTIONS  */
+	clear: both;
+	padding: 0px;
+	margin: 0px;
+   padding-bottom:10px;
+}
+
+.col {                  /*  COLUMN SETUP  */
+	display: block;
+	float:left;
+	margin: 1% 0 .5% .5%;
+   padding: 0px;
+}
+
+.col2 {                  /*  COLUMN SETUP  */
+	display: block;
+	float:left;
+	margin: 0px;
+   padding: 0px;
+}
+
+.fir{
+   display:none !important;
+}
+
+.sec{
+   width:100%;
+}
+
+.col:first-child { margin-left: 0; }
+.group:before,          /*  GROUPING  */
+.group:after { content:""; display:table; }
+.group:after { clear:both;}
+.group { zoom:1; /* For IE 6/7 */ }
+
+.span_title{width:20%;}
+.span_2_of_2 {          /*  GRID OF TWO  */
+	width: 30%;
+}
+
+.span_3_of_3 {          /*  GRID OF THREE  */
+	width: 33.3%;
+}
+
+.span_1_of_2 {
+	width: 49.2%;
+}
+
+
+
+.tb{
+	width: 103.5%;
+}
+
+.bord-top{
+   border-left: 1px solid #000099;
+   border-bottom: 1px solid #000099;
+   border-top: 1px solid #000099;
+   border-top-left-radius: 6px !important;
+}
+
+.bord-masc{
+   border-bottom: 1px solid #000099;
+   border-top: 1px solid #000099;
+}
+
+.bord-femm{
+   border-right: 1px solid #000099;
+   border-bottom: 1px solid #000099;
+   border-top: 1px solid #000099;
+   border-top-right-radius: 6px !important;
+   width:100% !important;
+}
+
+@media only screen and (max-width: 700px) {
+	.col {
+		margin: 1% 0 1% 0%;
+	}
+
+	.span_2_of_2, .span_1_of_2 , .span_3_of_3 { width: 100%; }
+   .blocklist{
+      width:90%;
+      margin: 10px !important;
+   }
+   .fir{
+      display:block !important;
+      width:40%;
+   }
+   
+   .sec{
+      width:60%;
+   }
+   
+   .sec-top{
+      padding-top:20px;
+   }
+   
+   .span_title{display:none;}
+   
+   .tb{
+		width: 100%;
+   }
+	
+   .bord-top{
+      border-left: 1px solid #000099;
+      border-top: 1px solid #000099;
+      border-top-left-radius: 0px !important;
+   }
+   
+   .bord-masc{
+      border-top: 1px solid #000099;
+      border-bottom: 0px;
+      border-right: 1px solid #000099;
+      border-left: 1px solid #000099;
+      border-top-right-radius: 7px !important;
+      border-top-left-radius: 7px !important;
+      width:98% !important;
+   }
+   
+   .bord-femm{
+      border-right: 1px solid #000099;
+      border-bottom: 1px solid #000099;
+      border-top: 1px solid #000099;
+      border-left: 1px solid #000099;
+      border-top-right-radius: 0px !important;
+      width:98% !important;
+   }
+}
+</style>''' + val
